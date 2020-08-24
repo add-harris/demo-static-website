cc43934de47e993103e44f1bf5eaa002f8899b14
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2020-08-24 21:12:03 +0100

    initial cmt

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+
+      <div id="slanted"></div>
+
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
+
+<style>
+
+  #slanted {
+    width: 0px;
+    height: 0px;
+    border-top: 50px solid orange;
+    /*border-bottom: 50px solid orange;*/
+    /*border-left: 50px solid orange;*/
+    border-right: 1000px solid transparent;
+    /*background-color: orange;*/
+  }
+
+</style>
