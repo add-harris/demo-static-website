0986a4f88359ce529d3d05442523b45299f11a82
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2020-08-25 14:10:29 +0100

    WIP: mega shrink on scroll app bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,19 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
+
+    <v-sheet
+      id="scrolling-techniques"
+      v-scroll.self="scrollFun"
+      class="overflow-y-auto"
+      max-height="500"
     >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
+      color="orange darken-1"
+      shrink-on-scroll
+      dark
+      flat
+      height="300px"
+      prominent
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn
@@ -57,32 +43,19 @@
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+
+
+
+
+      <v-main>
+        <v-container style="height: 1000px;">
+          <nuxt />
+        </v-container>
+      </v-main>
+
+    </v-sheet>
+
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -112,6 +85,28 @@ export default {
       rightDrawer: false,
       title: 'Vuetify.js'
     }
+  },
+  methods: {
+    scrollFun() {
+      console.log("scrolling")
+    }
   }
 }
 </script>
+
+<style>
+
+  #slanted {
+    width: 0px;
+    height: 0px;
+    position: fixed;
+    /*top: 0;*/
+    /*left: 0;*/
+    border-top: 50px solid orange;
+    /*border-bottom: 50px solid orange;*/
+    /*border-left: 50px solid orange;*/
+    border-right: 1500px solid transparent;
+    /*background-color: orange;*/
+  }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,12 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
 
-      <div id="slanted"></div>
+<!--      <div id="slanted2"></div>-->
+
+      <h1>Content</h1>
+      <h1>Content</h1>
+      <h1>Content</h1>
+      <h1>Content</h1>
 
     </v-flex>
   </v-layout>
@@ -22,13 +27,16 @@ export default {
 
 <style>
 
-  #slanted {
+  #slanted2 {
     width: 0px;
     height: 0px;
+    position: fixed;
+    top: 0;
+    left: 0;
     border-top: 50px solid orange;
     /*border-bottom: 50px solid orange;*/
     /*border-left: 50px solid orange;*/
-    border-right: 1000px solid transparent;
+    border-right: 1500px solid transparent;
     /*background-color: orange;*/
   }
 
