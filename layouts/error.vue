ef3f3af22fa8fac4721c8c86ea554e4c0f5848a6
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2020-11-16 10:15:33 +0000

    error path added, trial creating storage bucket

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,20 +1,20 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <ErrorPage :error="error"></ErrorPage>
   </v-app>
 </template>
 
 <script>
+
+import ErrorPage from '../components/ErrorPage'
+
 export default {
   layout: 'empty',
+
+  components: {
+    ErrorPage
+  },
+
   props: {
     error: {
       type: Object,
@@ -23,22 +23,9 @@ export default {
   },
   data () {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      // pageNotFound: '404 Not Found',
+      // otherError: 'An error occurred'
     }
   },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
-  }
 }
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
---
+++ pages/404.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <ErrorPage></ErrorPage>
+
+</template>
+
+<script>
+
+  import ErrorPage from '../components/ErrorPage'
+
+  export default {
+
+    name: '404',
+
+    layout: 'default',
+
+    components: {
+      ErrorPage
+    },
+
+    data () {
+      return {}
+    }
+
+  }
+
+</script>
